faa6b4cb781fe1d21524fbb0179f7ed991df50c4
NaimeDmts <naime.dmts@gmail.com>
2023-04-06 12:54:18 +0300

    3- Data Yapılarının Oluşturulması

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const AppController = (function(ProductCtrl, UICtrl) {
+const App = (function(ProductCtrl, UICtrl) {
  
+    return{
+        init: function(){
+            console.log('starting app...');
+            const products = ProductCtrl.getProducts();
+            
+            UICtrl.creatProductList(products);
+        }
+    }
 
 
 })(ProductController, UIController);
+
+App.init();
--- productController.js
+++ productController.js
@@ -1,5 +1,26 @@
 const ProductController = (function(){
+    const Product = function(id, name,price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
+    const data = {
+        products : [
+            {id:0, name: 'Monitör', price:100},
+            {id:1, name: 'Ram', price:30},
+            {id:2, name: 'Klavye', price:10},
+        ],
+        selectedProduct:null,
+        totalPrice:0
+    }
 
-    
+    return {
+        getProducts: function(){
+            return data.products;
+        },
+        getData: function(){
+            return data;
+        }
+    }
 })();
